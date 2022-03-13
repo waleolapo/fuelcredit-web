e090a308a2d303004e2a2b5b7f723dfb32f6c15f
Wale <wale4life@gmail.com>
2022-03-13 16:08:25 +0100

    Dashboard

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -15,7 +15,7 @@ const Dashboard = () => <>
                         to="/fuelcredit-web"
                         className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
                     >
-                    Logout
+                    Logout 
                     </Link>
                 </div>
             </div>
