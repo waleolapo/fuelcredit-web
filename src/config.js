49228dfd0082a7a80076c9828772b7f57ae39210
Wale <wale4life@gmail.com>
2022-03-14 09:07:59 +0100

    Updates

--- src/config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-    // APP_URL: process.env === 'production' ? 'https://3c86bbb289.lb2.amezmo.co/api' // : 'http://localhost:93/api',
-    APP_URL: 'https://3c86bbb289.lb2.amezmo.co/api' 
-}
-
-
-export default config
