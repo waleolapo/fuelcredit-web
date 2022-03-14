6e4f301df723e444541a0a64f07ce884035b11dc
Wale <wale4life@gmail.com>
2022-03-14 08:25:53 +0100

    production bug

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    APP_URL: process.env == 'production' ? 'https://3c86bbb289.lb2.amezmo.co/api' : 'http://localhost:93/api',
+    APP_URL: process.env === 'production' ? 'https://3c86bbb289.lb2.amezmo.co/api' : 'http://localhost:93/api',
 }
 
 
